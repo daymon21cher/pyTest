ef377c551359f130327087e06d0322e294e02a1d
Dmitriy Cheriapin <daymon_cher@mail.ru>
2024-05-12 22:54:03 +0300

    calculator add groupTest - TestCalculator

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,23 +2,24 @@ from src.main import Calculator
 import pytest
 
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5)
-    ]
-)
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
+class TestCalculator:
 
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 0.5),
+            (5, -1, -5)
+        ]
+    )
+    def test_divide(self, x, y, res):
+        assert Calculator().divide(x, y) == res
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 3),
-        (5, -1, 4)
-    ]
-)
-def test_add(x, y, res):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 3),
+            (5, -1, 4)
+        ]
+    )
+    def test_add(self, x, y, res):
+        assert Calculator().add(x, y) == res
